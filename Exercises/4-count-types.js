fcd7619bbda43f9d91485dc6ba2bc450542d0992
tshemsedinov <timur.shemsedinov@gmail.com>
2019-09-09 02:26:25 +0300

    Improve examples

---
+++ Exercises/4-count-types.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const countTypesInArray = array => {
+  const types = {};
+  for (const k of array) {
+    const type = typeof k;
+    types[type] = types[type] || 0;
+    types[type]++;
+  }
+  return types;
+};
+
+module.exports = { countTypesInArray };
