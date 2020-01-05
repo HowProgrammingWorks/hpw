74bffd8871fb41362abd36aaa6eefa0a03405afb
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-01-05 23:52:55 +0200

    Add more tests

---
+++ Exercises/7-log.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const { log: ln } = Math;
+const log = (base, n) => ln(n) / ln(base);
+
+module.exports = { ln, log };
---
+++ Exercises/8-not-found.js
@@ -0,0 +1,3 @@
+'use strict';
+
+module.exports = { f: null, g: undefined };
