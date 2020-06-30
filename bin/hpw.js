c9ca54119174e3ad35c1610eb168c7d71a473533
Oleksandr Avoiants <shhavel@gmail.com>
2020-06-30 23:12:54 +0300

    Update bin/hpw.js
    Co-authored-by: Timur Shemsedinov <timur.shemsedinov@gmail.com>

--- bin/hpw.js
+++ bin/hpw.js
@@ -74,8 +74,11 @@ const checkTarget = async (name, target, test) => {
   const msgLines = concolor`Lines: ${lines}(b,white)`;
   console.log(msgTarget + msgLength + msgLines);
   const [minLength, maxLength] = test.length || [];
-  if (targetLength > maxLength) throw new Error('Solution is too long: ' +
-    `no more than ${maxLength} characters expected.`);
+  if (targetLength > maxLength) {
+    throw new Error(
+      `Solution is too long: no more than ${maxLength} characters expected.`
+    );
+  }
   if (targetLength < minLength) throw new Error('Solution is too short: ' +
     `at least ${minLength} characters expected.`);
   let casesResult = 'No test cases';
